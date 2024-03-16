20af40d4fb6553aea0c0346c9f6b85970e16d234
askaaaaaaaaaa _ <bolbagantunahan22@gmail.com>
2024-03-16 17:31:50 +0500

    biar banyak commit

--- lib/home/homeScreen.dart
+++ lib/home/homeScreen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:auto_size_text/auto_size_text.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
