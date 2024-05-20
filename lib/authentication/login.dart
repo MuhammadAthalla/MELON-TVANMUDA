a45d09601f3eec41a3a987593bfc4bba240823e2
askaaaaaaaaaa _ <bolbagantunahan22@gmail.com>
2024-05-20 08:00:14 +0300

    forgot password

--- lib/authentication/login.dart
+++ lib/authentication/login.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:gradient_borders/gradient_borders.dart';
+import 'package:ujikomtvanmuda/authentication/forgot.dart';
 import 'package:ujikomtvanmuda/authentication/register.dart';
 import 'package:ujikomtvanmuda/home/homeScreen.dart';
 import 'package:ujikomtvanmuda/home/profile.dart';
@@ -49,196 +50,226 @@ class _LoginPageState extends State<LoginPage> {
 
     return Scaffold(
       body: Center(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Hello Again",
-                    style: GoogleFonts.poppins(
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold,
-                        foreground: Paint()..shader = linear)),
-                Text("Please sign in to your account",
-                    style: GoogleFonts.poppins(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        foreground: Paint()..shader = linear)),
-                SizedBox(
-                  height: 10,
-                ),
-                TextFormField(
-                  controller: emailController,
-                  decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      enabled: true,
-                      border: GradientOutlineInputBorder(
-                          gradient: LinearGradient(
-                              colors: [Color(0xFF20B263), Color(0x0ff78CC5A)]),
-                          width: 2,
-                          borderRadius:
-                              BorderRadius.all(const Radius.circular(44.0))),
-                      hintText: "Email",
-                      hintStyle: GoogleFonts.poppins(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Hello Again",
+                      style: GoogleFonts.poppins(
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
                           foreground: Paint()..shader = linear)),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return "Email cannot be empty";
-                    }
-                    if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
-                        .hasMatch(value)) {
-                      return ("Please enter a valid email");
-                    } else {
-                      return null;
-                    }
-                  },
-                  onSaved: (value) {
-                    emailController.text = value!;
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                ),
-                SizedBox(
-                  height: 22,
-                ),
-                TextFormField(
-                  controller: passwordController,
-                  obscureText: _isObscure3,
-                  decoration: InputDecoration(
-                      suffixIcon: IconButton(
-                        onPressed: () {
-                          setState(() {
-                            _isObscure3 = !_isObscure3;
-                          });
-                        },
-                        icon: Icon(_isObscure3
-                            ? Icons.visibility
-                            : Icons.visibility_off),
-                      ),
-                      filled: true,
-                      fillColor: Colors.white,
-                      enabled: true,
-                      border: GradientOutlineInputBorder(
-                          gradient: LinearGradient(
-                              colors: [Color(0xFF20B263), Color(0x0ff78CC5A)]),
-                          width: 2,
-                          borderRadius:
-                              BorderRadius.all(const Radius.circular(44.0))),
-                      hintText: "Password",
-                      hintStyle: GoogleFonts.poppins(
+                  Text("Please sign in to your account",
+                      style: GoogleFonts.poppins(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
                           foreground: Paint()..shader = linear)),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return "Password cannot be empty";
-                    } else {
-                      return null;
-                    }
-                  },
-                  onSaved: (value) {
-                    passwordController.text = value!;
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  height: 65,
-                  width: 350,
-                  decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                          color: Colors.black26,
-                          offset: Offset(0, 4),
-                          blurRadius: 5.0)
-                    ],
-                    gradient: LinearGradient(
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                      stops: [0.0, 1.0],
-                      colors: [
-                        Color(0x0ff20B263),
-                        Color(0x0ff78CC5A),
-                      ],
-                    ),
-                    color: Colors.deepPurple.shade300,
-                    borderRadius: BorderRadius.circular(44),
+                  SizedBox(
+                    height: 10,
                   ),
-                  child: ElevatedButton(
-                    style: ButtonStyle(
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20.0),
+                  TextFormField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        enabled: true,
+                        border: GradientOutlineInputBorder(
+                            gradient: LinearGradient(colors: [
+                              Color(0xFF20B263),
+                              Color(0x0ff78CC5A)
+                            ]),
+                            width: 2,
+                            borderRadius:
+                                BorderRadius.all(const Radius.circular(44.0))),
+                        hintText: "Email",
+                        hintStyle: GoogleFonts.poppins(
+                            foreground: Paint()..shader = linear)),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Email cannot be empty";
+                      }
+                      if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
+                          .hasMatch(value)) {
+                        return ("Please enter a valid email");
+                      } else {
+                        return null;
+                      }
+                    },
+                    onSaved: (value) {
+                      emailController.text = value!;
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  SizedBox(
+                    height: 22,
+                  ),
+                  TextFormField(
+                    controller: passwordController,
+                    obscureText: _isObscure3,
+                    decoration: InputDecoration(
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              _isObscure3 = !_isObscure3;
+                            });
+                          },
+                          icon: Icon(_isObscure3
+                              ? Icons.visibility
+                              : Icons.visibility_off),
+                        ),
+                        filled: true,
+                        fillColor: Colors.white,
+                        enabled: true,
+                        border: GradientOutlineInputBorder(
+                            gradient: LinearGradient(colors: [
+                              Color(0xFF20B263),
+                              Color(0x0ff78CC5A)
+                            ]),
+                            width: 2,
+                            borderRadius:
+                                BorderRadius.all(const Radius.circular(44.0))),
+                        hintText: "Password",
+                        hintStyle: GoogleFonts.poppins(
+                            foreground: Paint()..shader = linear)),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Password cannot be empty";
+                      } else {
+                        return null;
+                      }
+                    },
+                    onSaved: (value) {
+                      passwordController.text = value!;
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Align(
+                    alignment: Alignment.center,
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    const ForgotPassssword()));
+                      },
+                      child: Text(
+                        "Forgot Password?",
+                        style: GoogleFonts.poppins(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                            foreground: Paint()..shader = linear),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    height: 65,
+                    width: 350,
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.black26,
+                            offset: Offset(0, 4),
+                            blurRadius: 5.0)
+                      ],
+                      gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        stops: [0.0, 1.0],
+                        colors: [
+                          Color(0x0ff20B263),
+                          Color(0x0ff78CC5A),
+                        ],
+                      ),
+                      color: Colors.deepPurple.shade300,
+                      borderRadius: BorderRadius.circular(44),
+                    ),
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                        ),
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.transparent),
+                        shadowColor:
+                            MaterialStateProperty.all(Colors.transparent),
+                      ),
+                      onPressed: () {
+                        _signIn(emailController.text, passwordController.text);
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                          top: 10,
+                          bottom: 10,
+                        ),
+                        child: _isLoggingIn
+                            ? CircularProgressIndicator()
+                            : Text(
+                                'Login',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.white,
+                                ),
+                              ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Or Login With",
+                    style: GoogleFonts.poppins(
+                        fontWeight: FontWeight.bold, color: Colors.grey),
+                  ),
+                  SizedBox(
+                    height: 1,
+                  ),
+                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                    IconButton(
+                        onPressed: () {},
+                        icon: SvgPicture.asset('assets/svg/google_icon.svg')),
+                    IconButton(
+                        onPressed: () {},
+                        icon: SvgPicture.asset('assets/svg/facebook_icon.svg')),
+                  ]),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Dont Have An Account?",
+                        style: GoogleFonts.poppins(
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                      backgroundColor:
-                          MaterialStateProperty.all(Colors.transparent),
-                      shadowColor:
-                          MaterialStateProperty.all(Colors.transparent),
-                    ),
-                    onPressed: () {
-                      _signIn(emailController.text, passwordController.text);
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                        top: 10,
-                        bottom: 10,
-                      ),
-                      child: _isLoggingIn
-                          ? CircularProgressIndicator()
-                          : Text(
-                              'Login',
-                              style: TextStyle(
-                                fontSize: 18,
-                                color: Colors.white,
-                              ),
-                            ),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  "Or Login With",
-                  style: GoogleFonts.poppins(
-                      fontWeight: FontWeight.bold, color: Colors.grey),
-                ),
-                SizedBox(
-                  height: 1,
-                ),
-                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                  IconButton(
-                      onPressed: () {},
-                      icon: SvgPicture.asset('assets/svg/google_icon.svg')),
-                  IconButton(
-                      onPressed: () {},
-                      icon: SvgPicture.asset('assets/svg/facebook_icon.svg')),
-                ]),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "Dont Have An Account?",
-                      style: GoogleFonts.poppins(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    TextButton(
-                        onPressed: () {
-                          Navigator.pushNamed(context, RegisterPage.routeName);
-                        },
-                        child: Text(
-                          'Register',
-                          style: GoogleFonts.poppins(
-                              fontWeight: FontWeight.bold,
-                              foreground: Paint()..shader = linear),
-                        ))
-                  ],
-                )
-              ],
+                      TextButton(
+                          onPressed: () {
+                            Navigator.pushNamed(
+                                context, RegisterPage.routeName);
+                          },
+                          child: Text(
+                            'Register',
+                            style: GoogleFonts.poppins(
+                                fontWeight: FontWeight.bold,
+                                foreground: Paint()..shader = linear),
+                          ))
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
